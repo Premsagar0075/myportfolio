8af955134f1976bd87b85efce9eb7d276a882b0e
prem sagar <36024185+Premsagar0075@users.noreply.github.com>
2018-07-11 23:12:23 +0530

    adding social media links

--- blue-free/portfolio.php
+++ blue-free/portfolio.php
@@ -629,6 +629,30 @@ mysqli_close($conn);
 			        </div>
 			</section>
 			
+			
+			<!-- Social section -->
+			<section id="social" class="parallax">
+				<div class="overlay">
+					<div class="container">
+						<div class="row">
+						
+							<div class="sec-title text-center white wow animated fadeInDown">
+								<h2>FOLLOW ME</h2>
+								<p>Get in touch by follow me on facebook, twitter and insta.</p>
+							</div>
+							
+							<ul class="social-button">
+								<li class="wow animated zoomIn"><a href="https://www.facebook.com/prem.sagar.921"><i class="fa fa-thumbs-up fa-2x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.3s"><a href="https://twitter.com/imprem0075"><i class="fa fa-twitter fa-2x"></i></a></li>
+								<li class="wow animated zoomIn" data-wow-delay="0.6s"><a href="https://www.instagram.com/prem_sagar007/"><i class="fa fa-instagram fa-2x"></i></a></li>							
+							</ul>
+							
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- end Social section -->
+			
 		
 
 			
